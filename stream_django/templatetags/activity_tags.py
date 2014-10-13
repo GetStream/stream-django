eb0f35ad5185ddd66f539cead64ddbefa4246a8e
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-13 13:42:19 +0200

    fix template naming with prefix

--- stream_django/templatetags/activity_tags.py
+++ stream_django/templatetags/activity_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 def render_activity(context, activity, template_prefix=''):
     if template_prefix != '':
-        template_prefix = '_%s' % template_prefix
+        template_prefix = '%s_' % template_prefix
     if 'activities' in activity:
         template_name = "activity/aggregated/%s%s.html" % (template_prefix, activity['verb'])
     else:
