77ed3df3c98f553a1c3e5ed122f4f83e7abd38d7
Tommaso Barbugli <tbarbugli@gmail.com>
2014-11-11 12:56:05 +0100

    replace site_id with app_id

--- stream_django/context_processors.py
+++ stream_django/context_processors.py
@@ -4,5 +4,5 @@ from stream_django.client import stream_client
 def stream(request):
     context = {}
     context['STREAM_API_KEY'] = stream_client.api_key
-    context['STREAM_SITE_ID'] = stream_client.site_id
+    context['STREAM_APP_ID'] = stream_client.app_id
     return context
