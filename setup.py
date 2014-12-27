041eebabd087441d669cd72d97ce592454320d86
Tommaso Barbugli <tbarbugli@gmail.com>
2014-12-27 20:40:09 +0100

    add py 3k builds

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
