c2d543a3de566443a2c61761f9a190e915426fec
Matthisk Heimensen <m@tthisk.nl>
2015-10-30 15:11:06 +0100

    Return stream_client instead of binding it inside method (tests now passing)

--- stream_django/client.py
+++ stream_django/client.py
@@ -4,16 +4,13 @@ import stream
 from stream_django.conf import DJANGO_MAJOR_VERSION
 from django.core.exceptions import ImproperlyConfigured
 
-def init_client(mayRaise=False):
+def init_client(raise_config_error=False):
   if conf.API_KEY and conf.API_SECRET:
-      stream_client = stream.connect(
-          conf.API_KEY, conf.API_SECRET, location=conf.LOCATION, timeout=conf.TIMEOUT)
+      return stream.connect(conf.API_KEY, conf.API_SECRET, location=conf.LOCATION, timeout=conf.TIMEOUT)
   elif os.environ.get('STREAM_URL') is not None:
-      stream_client = stream.connect()
-  else:
-      stream_client = None
-      if mayRaise:
-        raise ImproperlyConfigured('Stream credentials are not set in your settings')
+      return stream.connect()
+  elif raise_config_error:
+      raise ImproperlyConfigured('Stream credentials are not set in your settings')
 
-stream_client = init_client(mayRaise=DJANGO_MAJOR_VERSION<1.7)
+stream_client = init_client(raise_config_error=DJANGO_MAJOR_VERSION<1.7)
 
