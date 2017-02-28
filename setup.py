6d163206b52413df04571cdcf0b3cbda39341aea
Tommaso Barbugli <tbarbugli@gmail.com>
2017-02-28 09:54:27 +0100

    release 1.3.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extras_require = {
 
 setup(
     name='stream-django',
-    version='1.3.1',
+    version='1.3.2',
     packages=['stream_django'],
     include_package_data=True,
     install_requires=requirements,
