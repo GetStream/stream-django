15fdbf9d6670c7ce95e8debe5e5993faa99c605d
Peter van Kampen <pterk@datatailors.com>
2018-12-24 13:06:14 +0100

    new release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extras_require = {
 
 setup(
     name='stream-django',
-    version='1.4.0',
+    version='1.5.0',
     packages=['stream_django'],
     include_package_data=True,
     install_requires=requirements,
