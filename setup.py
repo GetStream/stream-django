74b7d7164619d2e70cd40f50c3f58f161a3ebe96
Peter van Kampen <pterk@datatailors.com>
2019-06-13 16:58:20 +0200

    Support django 2.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 if sys.version_info < (3, 0, 0):
     django = 'django>=1.8.10,<2.0'
 else:
-    django = 'django>=1.8.10,<2.2'
+    django = 'django>=1.8.10,<3.0'
 
 requirements = [
     django,
