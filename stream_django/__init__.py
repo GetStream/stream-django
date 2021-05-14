38582178e8408994a610c241e2e73991f951f129
jason <jason@zzq.org>
2021-05-14 10:44:09 -0600

    support for Django 3.2
    - * app config is enabled by default. `default_app_config` variable deprecated and removed.
    
    - note: slugify now removes leading and trailing dashes and underscores
    
    - no changes needed for database backend api
    - no changes needed for admin backoffice
    - no changes needed for gis
    - no changes needed for postgres, mysql version support update
    - intcomma intword unused
    - argon2-cffi unused
    - cache unused for USE_L10N setting
    - ForeignKey.validate unused
    - limited_parse_qsl unused
    - TestContextDecorator unused
    - SessionMiddleware unused
    - Field equality operator distiguishes inherited fields
    - django.core.files.locks.lock() unused
    - password reset token invalidation n/a
    - makemessages command n/a
    - ReadOnlyPasswordHashField unused
    - changes to cache get_many/get_or_set/has_key/incr/decr/incr_version/decr_version n/a
    - deepcopy unused
    - BaseCommand unused
    - EmailValidator unused
    - TransactionTestCase unused
    - Memcache unused
    - CookieStorage unused

--- stream_django/__init__.py
+++ stream_django/__init__.py
@@ -1,3 +1,8 @@
-from stream_django.feed_manager import feed_manager
-default_app_config = 'stream_django.apps.StreamDjangoConfig'
+import django
+from stream_django.feed_manager import feed_manager  # noqa
 
+major, minor, _ = (int(i) for i in django.__version__.split('.'))
+
+if major < 3 or (major == 3 and minor < 2):
+    # deprecated as of Django 3.2
+    default_app_config = 'stream_django.apps.StreamDjangoConfig'
