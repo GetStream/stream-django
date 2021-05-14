6e79db2666c7f6151f06f7743c024439b7a95ea8
Szymon Zmilczak <szymon1313@gmail.com>
2021-05-14 15:52:59 +0200

    Support for django 3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
 if sys.version_info < (3, 0, 0):
     django = "django>=1.11.29,<2.0"
 else:
-    django = "django>=2.0,<3.2"  # checked https://docs.djangoproject.com/en/dev/releases/3.1/#backwards-incompatible-3-1
+    django = "django>=2.0,<3.3"  # checked https://docs.djangoproject.com/en/dev/releases/3.2/#backwards-incompatible-3-2
 
 requirements = [django, "stream-python>=3.0.1", "pytz"]
 
