6aef9f09f9e663779dda3fb502702fccea7e8fd5
Tadej Krevh <tadej.krevh@gmail.com>
2021-08-13 13:41:59 +0200

    Fix version checking for Django 3.2 (#109)

--- stream_django/__init__.py
+++ stream_django/__init__.py
@@ -1,7 +1,8 @@
 import django
 from stream_django.feed_manager import feed_manager  # noqa
 
-major, minor, _ = (int(i) for i in django.__version__.split('.'))
+version_list = [int(i) for i in django.__version__.split('.')]
+major, minor = version_list[0], version_list[1]
 
 if major < 3 or (major == 3 and minor < 2):
     # deprecated as of Django 3.2
